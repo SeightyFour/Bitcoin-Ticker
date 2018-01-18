1008b17f25389fcfb2cd9a1f627dd041e1c49ecf
Since Eighty Four <im.mario.rodrigues@gmail.com>
2018-01-18 13:05:33 +0000

    Create currency struct and implemented UIPickerView delegates

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -1,18 +1,26 @@
-//
-//  ViewController.swift
-//  BitcoinTicker
-//
-//  Created by Angela Yu on 23/01/2016.
-//  Copyright © 2016 London App Brewery. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+struct Bitcoin: Decodable {
+	let ask: Double
+	let bid: Double
+	let volume: Double
+	let averages: [String: Double]
+	
+// We don't need this contructor anymore with Swift 4
+//
+//	init(json: [String: Any]) {
+//		ask = json["ask"] as? Double ?? 0
+//		bid = json["bid"] as? Double ?? 0
+//		volume = json["volume"] as? Double ?? 0
+//	}
+	
+}
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
-    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let jsonURLString = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-    var finalURL = ""
+	var finalURL: URL?
 
     //Pre-setup IBOutlets
     @IBOutlet weak var bitcoinPriceLabel: UILabel!
@@ -22,64 +30,57 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+		currencyPicker.dataSource = self
+		currencyPicker.delegate = self
        
     }
 
     
     //TODO: Place your 3 UIPickerView delegate methods here
     
-    
-    
-
-    
-    
-    
-//    
-//    //MARK: - Networking
-//    /***************************************************************/
-//    
-//    func getWeatherData(url: String, parameters: [String : String]) {
-//        
-//        Alamofire.request(url, method: .get, parameters: parameters)
-//            .responseJSON { response in
-//                if response.result.isSuccess {
-//
-//                    print("Sucess! Got the weather data")
-//                    let weatherJSON : JSON = JSON(response.result.value!)
-//
-//                    self.updateWeatherData(json: weatherJSON)
-//
-//                } else {
-//                    print("Error: \(String(describing: response.result.error))")
-//                    self.bitcoinPriceLabel.text = "Connection Issues"
-//                }
-//            }
-//
-//    }
-//
-//    
-//    
-//    
-//    
-//    //MARK: - JSON Parsing
-//    /***************************************************************/
-//    
-//    func updateWeatherData(json : JSON) {
-//        
-//        if let tempResult = json["main"]["temp"].double {
-//        
-//        weatherData.temperature = Int(round(tempResult!) - 273.15)
-//        weatherData.city = json["name"].stringValue
-//        weatherData.condition = json["weather"][0]["id"].intValue
-//        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
-//        }
-//        
-//        updateUIWithWeatherData()
-//    }
-//    
-
-
-
-
+	func numberOfComponents(in pickerView: UIPickerView) -> Int {
+		return 1
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+		return currencyArray.count
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+		return currencyArray[row]
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+		
+		guard let finalURL = URL(string: String(jsonURLString + currencyArray[row])) else {return}
+		
+		//MARK: - Vanilla Networking
+		
+		URLSession.shared.dataTask(with: finalURL) { (data, response, err) in
+			// check for error
+			// check for response status 200 OK
+			
+			guard let data = data else {return}
+			
+			//let dataAsString = String(data: data, encoding: .utf8)
+			
+			do {
+				//MARK: - Vanilla JSON Parsing
+				let BTC = try JSONDecoder().decode(Bitcoin.self, from: data)
+				
+				DispatchQueue.main.async {
+					self.bitcoinPriceLabel.text = String(BTC.ask)
+				}
+				
+				// guard let dailyAverage = BTC.averages["day"] else {return}
+				// print(dailyAverage)
+				
+			}catch let jsonError{
+				print("Error serializing json:", jsonError)
+			}
+			
+		}.resume()
+		
+	}
 }
 
